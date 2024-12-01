36837d14ba9edcbedb9021ee064cc40cde0915ca
Romeu Sorionaet <romeusoares14569@gmail.com>
2024-12-01 11:28:07 -0300

    style: defined dynamic style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,8 +8,49 @@ export default {
   ],
   theme: {
     extend: {
+      variants: {
+        extend: {
+          content: ['before', 'after'],
+        },
+      },
+
       fontFamily: {
         inter: 'var(--font-inter)',
+      },
+
+      fontSize: {
+        custom_base_text: 'clamp(0.70rem, 1vw, 1.1rem)',
+        custom_patreon: 'clamp(0.90rem, 1.3vw, 1.8rem)',
+      },
+
+      padding: {
+        custom_container_header: 'clamp(1.25rem, 2.5vw, 3rem)',
+        custom_1: 'clamp(0.60rem, 0.80vw, 1.5rem)',
+      },
+
+      spacing: {
+        '16': '4rem',
+      },
+
+      screens: {
+        md_middle: '850px',
+      },
+
+      animation: {
+        'rise-from-bottom': 'riseFromBottom 0.4s ease-out forwards',
+      },
+
+      keyframes: {
+        riseFromBottom: {
+          '0%': {
+            transform: 'translateY(200%)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: '1',
+          },
+        },
       },
     },
   },
